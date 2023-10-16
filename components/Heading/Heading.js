7e24f0916cefcb4cfe98cfa14e6f517a55d5b166
Victor Hornets <victorhornets@VictorHornets.lan>
2023-10-16 10:25:13 +0300

    changed heading padding

--- components/Heading/Heading.js
+++ components/Heading/Heading.js
@@ -4,7 +4,7 @@ import { getTextAlign, getFontsSizeForHeading } from "utils/fonts";
 export const Heading = ({ textAlign, content, level = 1 }) => {
   const tag = React.createElement(`h${level}`, {
     dangerouslySetInnerHTML: { __html: content },
-    className: `font-heading max-w-2xl mx-auto my-12 font-semibold text-gray-800 leading-normal ${getFontsSizeForHeading(
+    className: `font-heading max-w-2xl mx-auto my-12 font-semibold text-gray-800 leading-normal px-4 ${getFontsSizeForHeading(
       level
     )} ${getTextAlign(textAlign)}`,
   });
